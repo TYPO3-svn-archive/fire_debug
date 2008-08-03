197fb69ca291be814c6ac0526d909d0207db96c3
ischmittis <ischmittis@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-03 18:22:25 +0000

    * Updated ext_emconf.php after upload to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fire_debug/trunk@10921 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "fire_debug"
 #
-# Auto generated 03-08-2008 20:10
+# Auto generated 03-08-2008 20:22
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.9.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"2a67";s:23:"class.tx_fire_debug.php";s:4:"9830";s:12:"ext_icon.gif";s:4:"9a17";s:17:"ext_localconf.php";s:4:"c34d";s:12:"fire.tpl.php";s:4:"d6c1";s:14:"doc/manual.sxw";s:4:"15b5";s:19:"doc/wizard_form.dat";s:4:"b4a4";s:20:"doc/wizard_form.html";s:4:"69aa";s:29:"hooks/class.user_tslib_fe.php";s:4:"5d55";s:18:"lib/FireExport.php";s:4:"bc4d";s:21:"lib/FirePHP.class.php";s:4:"0856";s:15:"lib/FireVar.php";s:4:"b7c6";s:10:"lib/fb.php";s:4:"3bc4";}',
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"3578";s:23:"class.tx_fire_debug.php";s:4:"9830";s:12:"ext_icon.gif";s:4:"9a17";s:17:"ext_localconf.php";s:4:"c34d";s:12:"fire.tpl.php";s:4:"d6c1";s:14:"doc/manual.sxw";s:4:"86e0";s:19:"doc/wizard_form.dat";s:4:"b4a4";s:20:"doc/wizard_form.html";s:4:"69aa";s:29:"hooks/class.user_tslib_fe.php";s:4:"5d55";s:18:"lib/FireExport.php";s:4:"bc4d";s:21:"lib/FirePHP.class.php";s:4:"0856";s:15:"lib/FireVar.php";s:4:"b7c6";s:10:"lib/fb.php";s:4:"3bc4";}',
 	'suggests' => array(
 	),
 );
